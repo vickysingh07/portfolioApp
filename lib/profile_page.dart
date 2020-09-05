d73176fd87d7e777d2562d549768df8ddd3bab4b
Vicky <vicky1910098@akgec.ac.in>
2020-09-05 13:12:05 +0530

    First commit

---
+++ lib/profile_page.dart
@@ -0,0 +1,320 @@
+// ignore: avoid_web_libraries_in_flutter
+//import 'dart:html' as html;
+
+import 'package:flutter/material.dart';
+import 'package:webappp/responsive_widget.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key key}) : super(key: key);
+
+  List<Widget> navButtons() => [
+        NavButton(
+          text: "about",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+        NavButton(
+          text: "work",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+        NavButton(
+          text: "contact",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+        ),
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                  padding: const EdgeInsets.all(20),
+                  children: navButtons(),
+                ),
+              )
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            duration: Duration(seconds: 1),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  NavHeader(navButtons: navButtons()),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  ProfileInfo(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  SocialInfo(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavHeader extends StatelessWidget {
+  final List<Widget> navButtons;
+
+  const NavHeader({Key key, this.navButtons}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          VickyDot(),
+//          Spacer(),
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: navButtons,
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class VickyDot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          "Vicky",
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        AnimatedContainer(
+          duration: Duration(seconds: 1),
+          height: 8,
+          width: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.orange,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  profileImage(context) => Container(
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.deepOrange,
+//            borderRadius: BorderRadius.circular(40),
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage("Vicky.JPG"),
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+
+  final profileData = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Hi there! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.orange),
+      ),
+      Text(
+        "Vicky\nSingh",
+        textScaleFactor: 5,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "I am a beginner in app development field."
+        "I got 9.23 sgpa in first semester and 8.7sgpa in second semester."
+        "My skills are C++, data structure and problem solving."
+        "My goal is to be a successful app developer.",
+        softWrap: true,
+        textScaleFactor: 1.5,
+        style: TextStyle(color: Colors.white70),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("Resume"),
+            color: Colors.red,
+            onPressed: () {
+              //html.window.open(
+              //  "https://google-developers.appspot.com/community/experts/directory/profile/profile-pawan_kumar",
+              // "GDE");
+            },
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          OutlineButton(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            shape: StadiumBorder(),
+            child: Text("Say Hi!"),
+            color: Colors.red,
+            onPressed: () {
+              // html.window.open("https://pawan.live", "Pk");
+            },
+            padding: EdgeInsets.all(10),
+          )
+        ],
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[profileImage(context), profileData],
+      ),
+      smallScreen: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          profileImage(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          profileData
+        ],
+      ),
+    );
+  }
+}
+
+class SocialInfo extends StatelessWidget {
+  List<Widget> socialMediaWidgets() {
+    return [
+      NavButton(
+        text: "Github",
+        onPressed: () {
+          //html.window.open("https://github.com/vickysingh07", "Git");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Twitter",
+        onPressed: () {
+          //html.window.open("https://twitter.com/imthepk", "Twitter");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Facebook",
+        onPressed: () {
+          //html.window.open("https://facebook.com/vickysingh", "Fb");
+        },
+        color: Colors.blue,
+      ),
+    ];
+  }
+
+  Widget copyRightText() => Text(
+        "Vicky Singh ©️2020",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.grey,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: socialMediaWidgets(),
+          ),
+          copyRightText(),
+        ],
+      ),
+      smallScreen: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ...socialMediaWidgets(),
+          copyRightText(),
+        ],
+      ),
+    );
+  }
+}
